56ea3f598c22ed0567c90b9ab463e8d8828c6963
Benedek Ákos Marcell <benedek.akos.marcell@gmail.com>
2022-11-08 10:33:59 +0100

    Finish

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-var i = 0;
 document.addEventListener('DOMContentLoaded',()=>{
     document.getElementById('allabc').addEventListener('click',async()=>{
         let response = await fetch('users.json');
@@ -15,48 +14,78 @@ document.addEventListener('DOMContentLoaded',()=>{
                 return 0;
             }
         });
-        i=1;
-        dataShow(abcall,i);
+        let userList = document.getElementById('results');
+        userList.textContent = '';
+        let table = document.getElementById('restable');
+        table.innerHTML = '';
+        for (let p of abcall){
+            let li = document.createElement('li');
+            li.innerHTML = p.lastName.toUpperCase() + ' ' + p.firstName ;
+            userList.appendChild(li);
+        };
     });
     document.getElementById('connection').addEventListener('click',async()=>{
+        let response = await fetch('users.json');
+        let eredmeny = await response.json();
+        let usertable = eredmeny.users.sort(function(a, b){
+            let first = a.username.toUpperCase();
+            let second = b.username.toUpperCase();
 
+            if(first < second){
+                return -1;
+            }else if( first > second){
+                return 1;
+            }else{
+                return 0;
+            }
+        });
+        let userList = document.getElementById('results');
+        userList.textContent = '';
+        let table = document.getElementById('restable');
+        table.innerHTML = '';
+        for (let p of usertable){
+            let row = document.createElement('tr');
+
+            let name = document.createElement('td');
+            let email = document.createElement('td');
+            let phone = document.createElement('td');
+
+            name.textContent = p.username;
+            email.textContent = p.email;
+            phone.textContent = p.phone;
+
+            row.appendChild(name);
+            row.appendChild(email);
+            row.appendChild(phone);
+            table.appendChild(row);
+        }
     });
     document.getElementById('heightweight').addEventListener('click',async()=>{
         let response = await fetch('users.json');
         let eredmeny = await response.json();
         let height = eredmeny.users.filter(e => e.height >= parseFloat(document.getElementById('tall').value))
-        i=3;
-        dataShow(height,i);
-    });
-    document.getElementById('brown').addEventListener('click',async()=>{
-        let response = await fetch('users.json');
-        let eredmeny = await response.json();
-        let brown = eredmeny.users.filter(e => e.eyeColor === 'Brown');
-        i=4;
-        dataShow(brown,i);
-    });
-    function dataShow(users,fid){
-    let userList = document.getElementById('results');
-    userList.textContent = '';
-    if (fid == 1){
-        for (let p of users){
-            let li = document.createElement('li');
-            li.innerHTML = p.lastName + ' ' + p.firstName ;
-            userList.appendChild(li);
-        };
-    } else if (fid == 2){
-
-    } else if(fid == 3){
+        let userList = document.getElementById('results');
+        userList.textContent = '';
+        let table = document.getElementById('restable');
+        table.innerHTML = '';
         let ot = 0;
-        for (let p of users){
+        for (let p of height){
             ot += p.weight;
         };
         let li = document.createElement('li');
         li.innerHTML = ot+"kg az össz tömegük";
         userList.appendChild(li);
-    } else if(fid == 4){
+    });
+    document.getElementById('brown').addEventListener('click',async()=>{
+        let response = await fetch('users.json');
+        let eredmeny = await response.json();
+        let brown = eredmeny.users.filter(e => e.eyeColor === 'Brown');
+        let userList = document.getElementById('results');
+        userList.textContent = '';
+        let table = document.getElementById('restable');
+        table.innerHTML = '';
         let li = document.createElement('li');
-        li.innerHTML = users.length+" barna szemű van";
+        li.innerHTML = brown.length+" barna szemű van";
         userList.appendChild(li);
-    }};
+    });
 });
